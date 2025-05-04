3b14f276dbed938b01cdbbec2e69939f34e6d33b
Palich <160468183+masterSorokoum@users.noreply.github.com>
2025-05-04 23:32:14 +0700

    Create ShowBitsDemo.java
    Отображение работы ShowBits

---
+++ ShowBitsDemo.java
@@ -0,0 +1,23 @@
+/*
+   Упражнение 5.4.
+   Sorokoumov
+*/
+class ShowBitsDemo {
+    public static void main(String[] args) {
+        ShowBits b = new ShowBits(8);
+        ShowBits i = new ShowBits(32);
+        ShowBits li = new ShowBits(64);
+
+        System.out.println("Двоичное представление значения 123: ");
+        b.show(123);
+
+        System.out.println("\nДвоичное представление значения 87987: ");
+        i.show(87987);
+
+        System.out.println("\nДвоичное представление значения 237658768: ");
+        li.show(237658768);
+
+        System.out.println("\nМладшие 8 битов значения 87987: ");
+        b.show(87987);
+    }
+}
